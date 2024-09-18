7bc8028231de3df5926859dbebe90c3e600f18be
Jim Samios <dsamios@hey.com>
2024-09-18 18:10:33 -0500

    Move to pokeapi file methods that will be used by all resources.

--- pokeapi/locations_resource.go
+++ pokeapi/locations_resource.go
@@ -3,9 +3,6 @@ package pokeapi
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
 
 	"github.com/jimsam/pokedexcli/pokecache"
 )
@@ -22,58 +19,22 @@ type MapResponse struct {
 }
 
 func (r MapResponse) GetResource(resourceURL string, cache *pokecache.Cache) (interface{}, error) {
-	url, err := url.Parse(resourceURL)
-	if err != nil {
-		return r, fmt.Errorf("The was an error with parsing %w", err)
-	}
-	query := url.Query()
-	if len(query) == 0 {
-		query.Add("limit", "0")
-		query.Add("offset", "0")
-		url.RawQuery = query.Encode()
-	}
-
-	data, found := cache.Get(url.String())
+	data, found := cache.Get(resourceURL)
+	var err error
 	if !found {
 		fmt.Println("Fetching from web...")
-		data, err = fetchFromWeb(url.String(), cache)
+		data, err = fetchFromWeb(resourceURL, cache)
 	}
 	err = json.Unmarshal(data, &r)
 	if err != nil {
 		return r, err
 	}
 	r.Resource = "locations"
-	printData(r)
+	printLocations(r)
 	return r, nil
 }
 
-func fetchFromWeb(resourceURL string, cache *pokecache.Cache) ([]byte, error) {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", resourceURL, nil)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	body, err := io.ReadAll(res.Body)
-	defer res.Body.Close()
-	if err != nil {
-		return []byte{}, err
-	}
-
-	if res.StatusCode >= 200 && res.StatusCode < 300 {
-		cache.Add(resourceURL, body)
-		return body, nil
-	} else {
-		return []byte{}, fmt.Errorf("The was a response with %d code with message: %v", res.StatusCode, res.Body)
-	}
-}
-
-func printData(r MapResponse) error {
+func printLocations(r MapResponse) error {
 	for _, record := range r.Results {
 		fmt.Println(record.Name)
 	}
--- pokeapi/pokeapi.go
+++ pokeapi/pokeapi.go
@@ -2,6 +2,9 @@ package pokeapi
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"net/http"
 	"time"
 
 	"github.com/jimsam/pokedexcli/pokecache"
@@ -10,11 +13,12 @@ import (
 const baseURL = "https://pokeapi.co/api/v2/"
 
 var resources = map[string]string{
-	"map": baseURL + "location",
+	"map":     baseURL + "location",
+	"explore": baseURL + "location-area/",
 }
 
 type FetchData interface {
-	GetResource(resource string, cache *pokecache.Cache) (interface{}, error)
+	GetResource(resourceURL string, cache *pokecache.Cache) (interface{}, error)
 }
 
 var cache *pokecache.Cache
@@ -23,12 +27,13 @@ func init() {
 	cache = pokecache.NewCache(time.Minute * 5)
 }
 
-func ProcessRequest(r FetchData, resource string, lastResponse *any) error {
-	resourceURL, err := getProperUrl(resource, *lastResponse)
+func ProcessRequest(r FetchData, resource string, lastResponse *any, args []string) error {
+	resourceURL, err := getProperUrl(resource, *lastResponse, args)
 	if err != nil {
 		return err
 	}
 
+	fmt.Println(resourceURL)
 	*lastResponse, err = r.GetResource(resourceURL, cache)
 	if err != nil {
 		return err
@@ -36,7 +41,7 @@ func ProcessRequest(r FetchData, resource string, lastResponse *any) error {
 	return nil
 }
 
-func getProperUrl(resource string, lastResponse any) (string, error) {
+func getProperUrl(resource string, lastResponse any, args []string) (string, error) {
 	if lastResponse != nil {
 		switch v := lastResponse.(type) {
 		case MapResponse:
@@ -56,5 +61,40 @@ func getProperUrl(resource string, lastResponse any) (string, error) {
 	if !found {
 		return "", errors.New("Requested resource was not found!")
 	}
+	if len(args) > 1 {
+		resourceURL += args[1]
+		return resourceURL, nil
+	}
+	resourceURL = addFirstPagePagination(resourceURL)
 	return resourceURL, nil
+}
+
+func addFirstPagePagination(url string) string {
+	return url + "?offset=0&limit=20"
+}
+
+func fetchFromWeb(resourceURL string, cache *pokecache.Cache) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", resourceURL, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		return []byte{}, err
+	}
+
+	if res.StatusCode >= 200 && res.StatusCode < 300 {
+		cache.Add(resourceURL, body)
+		return body, nil
+	} else {
+		return []byte{}, fmt.Errorf("The was a response with %d code with message: %v", res.StatusCode, res.Body)
+	}
 }
